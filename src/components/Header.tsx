71d9a1efe0d8f016ff9f8e71b7ca3fd8a99ac695
Morten Knudsen <mort1k@hotmail.com>
2025-05-25 13:08:10 +0200

    Back button from events to items.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 interface Props {
-    headline: string;
+    showEvents: boolean;
+    onItemsButtonClick: () => void;
 }
 
-function Header({ headline }: Props) {
+function Header({ showEvents, onItemsButtonClick }: Props) {
     return (
-        <header>
-            <h1>Taghub file explorer</h1>
-            <h2>{headline}</h2>
+        <header className="app-header">
+            <h1 className="app-header-h1">Taghub file explorer</h1>
+            <button 
+                className="app-header-button"
+                onClick={onItemsButtonClick}
+            >
+                Items
+            </button>
+            {showEvents ? 
+                <button className="app-header-button">\Events</button> : null}
         </header>
     );
 }
--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -8,7 +8,6 @@ import EventList from './EventList';
 
 function Start() {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [activeHeadline, setActiveHeadline] = useState<string>('');
     const [selectedProjectUuid, setSelectedProjectUuid] = useState<string | null>(null);
     const [selectedItemEpcString, setSelectedItemEpcString] = useState<string | null>(null);
     const [showItemEvents, setShowItemEvents] = useState<boolean>(false);
@@ -19,7 +18,6 @@ function Start() {
 
     function handleProjectSelection(projectUuid: string | null) {
         setSelectedProjectUuid(projectUuid);
-        setActiveHeadline('Items');
         setSelectedItemEpcString(null);
         setShowItemEvents(false);
         console.log('Selected project: ', projectUuid);
@@ -27,34 +25,42 @@ function Start() {
 
     function handleItemSelection(itemEpcString: string | null) {
         setSelectedItemEpcString(itemEpcString);
-        setActiveHeadline('Events');
         setShowItemEvents(true);
         console.log('project uuid: ', selectedProjectUuid);
         console.log('Setting epcString for item selection = ', itemEpcString);
+    }
+
+    function handleItemsButtonClick() {
+        setShowItemEvents(false);
     }
         
     if (!isLoggedIn) {
         return <Login onLogin={handleLogin} />;
     } else {
         return (
-            <div className="app-div">
-                <Header headline={activeHeadline} />
-                <ProjectList 
-                    onProjectSelection={handleProjectSelection} 
-                />
-                {!showItemEvents ? (
-                    <ItemList 
-                        projectUuid={selectedProjectUuid} 
-                        onItemSelection={handleItemSelection} 
+            <>
+              
+                    <Header 
+                        showEvents={showItemEvents}
+                        onItemsButtonClick={handleItemsButtonClick} 
                     />
-                ) : (
-                    <EventList 
-                        projectUuid={selectedProjectUuid}
-                        itemEpcString={selectedItemEpcString}
+                    <ProjectList 
+                        onProjectSelection={handleProjectSelection} 
                     />
-                )
-                }
-            </div>
+                    {!showItemEvents ? (
+                        <ItemList 
+                            projectUuid={selectedProjectUuid} 
+                            onItemSelection={handleItemSelection} 
+                        />
+                    ) : (
+                        <EventList 
+                            projectUuid={selectedProjectUuid}
+                            itemEpcString={selectedItemEpcString}
+                        />
+                    )
+                    }
+            
+            </>
         );}
 }
 
