91e89dbdb8d1c00080989d1ca08f046e2002e5bc
Morten Knudsen <mort1k@hotmail.com>
2025-05-25 17:45:56 +0200

    Updated header buttons.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,17 +9,23 @@ function Header({ showEvents, onItemsButtonClick, onLogOutButtonClick }: Props) 
     return (
         <header className="app-header">
             <h1 className="app-header-h1">Taghub file explorer</h1>
-            <button 
-                className="app-header-btn"
-                onClick={onItemsButtonClick}
-            >
-                Items
-            </button>
-            {showEvents ? <button className="app-header-btn">\Events</button> : null}
-            <button 
-                className="app-header-btn log-out-btn"
-                onClick={onLogOutButtonClick}
-            >Log out</button>
+            <div className="app-header-btn-div">
+                <div>
+                    <button 
+                        className="app-header-btn"
+                        onClick={onItemsButtonClick}
+                    >
+                        Items
+                    </button>
+                    {showEvents ? <button className="app-header-btn">\Events</button> : null}
+                </div>
+                <button 
+                    className="app-header-btn log-out-btn"
+                    onClick={onLogOutButtonClick}
+                >
+                    Log out
+                </button>
+            </div>
         </header>
     );
 }
